a10b15ded1d0f3579cd9dab759c781af7367c0f6
KaustubhNayak005 <nayakkaustubh@gmail.com>
2026-08-26 01:58:53 +0530

    Add testing framework and unit tests

--- tests/test_train_loop.py
+++ tests/test_train_loop.py
@@ -6,7 +6,7 @@ def test_train_script_runs():
     # We can't fully run it if data is missing, but we can verify imports work.
     
     proj_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    script_path = os.path.join(proj_root, 'src', 'experiments', 'train.py')
+    script_path = os.path.join(proj_root, 'src', 'train.py')
     
     # Run with --help to verify imports and argparse
     result = subprocess.run(['python', script_path, '--help'], capture_output=True, text=True)
